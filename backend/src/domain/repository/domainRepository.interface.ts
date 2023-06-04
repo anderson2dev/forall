374f4a82a60155fe1d13562d6a1fa283457252a9
anderson2dev <subscriberandersom@gmail.com>
2023-06-04 04:50:42 -0300

    feat: Adds custom selection  on queries

--- backend/src/domain/repository/domainRepository.interface.ts
+++ backend/src/domain/repository/domainRepository.interface.ts
@@ -1,7 +1,19 @@
-export interface IGenericDomainRepository<T> {
-  findAll(): Promise<T[]>;
-  findOne(id: string): Promise<T>;
+type genericDomainEntity = {
+  id: string;
+};
+
+/*
+TODO: create an type for select which is compatible with FindOptionsSelectProperty from typeorm but could also be used interchangeble
+ */
+
+export interface IGenericDomainRepository<T extends genericDomainEntity> {
+  findAll(
+    queryObj?: Omit<NonNullable<Partial<T>>, 'id'>,
+    select?: any,
+    size?: number,
+  ): Promise<T[] | NonNullable<Partial<T>>[]>;
+  findOne(id: string): Promise<T | NonNullable<Partial<T>>>;
   delete(id: string): Promise<void>;
-  update(id: string, updateObj: Partial<T>): Promise<void>;
+  update(id: string, updateObj: NonNullable<Partial<T>>): Promise<void>;
   insert(entity: T): Promise<void>;
 }
