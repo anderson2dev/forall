61502eee7386369086f1de0069a9600830fb221e
anderson2dev <subscriberandersom@gmail.com>
2023-07-04 07:11:34 -0300

    chore: adds topic status

---
+++ backend/src/infrastructure/database/migrations/1688465414126-topic-status.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class TopicStatus1688465414126 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    try {
+      await queryRunner.query(`
+          CREATE TYPE topic_status AS ENUM ('OPEN', 'CLOSED');
+        `);
+      await queryRunner.query(
+        `ALTER TABLE "topic" ADD "status" topic_status NOT NULL DEFAULT 'OPEN'`,
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    try {
+      await queryRunner.query(`ALTER TABLE "topic" DROP COLUMN "status"`);
+      await queryRunner.query(`DROP TYPE topic_status`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
